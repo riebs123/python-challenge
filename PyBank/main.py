a5d1d332f4c540718e60fc4d5fe53a3cc37198c1
riebs123 <tannerriebel@hotmail.com>
2021-01-23 17:58:51 -0600

    final ouputs

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,85 @@
-print("Hello from PyBank")
+import os
+import csv
+from collections import Counter
+from collections import defaultdict
+
+csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+
+profit_list = []
+profit_month = []
+
+with open(csvpath) as csvfile:
+
+    total_profitlosses_change = 0
+    total_profitlosses= 0
+    total_months = 0
+    max_profit = 0
+    min_profit = 0
+
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        total_months += 1
+        date = row[0]
+        profitlosses = int(row[1])
+
+        profit_list.append(profitlosses)
+        profit_month.append(date)
+        total_profitlosses += profitlosses 
+    
+
+    
+    profit_change = [y-x for x, y in zip(profit_list, profit_list[1:])]
+    
+    for i, x in enumerate(profit_change): #utilize this for getting min and max
+        
+        if x > max_profit:
+            max_profit = x
+            max_profit_month = i+1
+        if x < min_profit:
+            min_profit = x
+            min_profit_month = i+1
+        
+        total_profitlosses_change += x
+    
+
+    # Average Change 
+
+    average_change = round((total_profitlosses_change/85),2) 
+
+    #Greatest Increase and Decrease months
+
+    for i, x in enumerate(profit_month):
+        if i == max_profit_month:
+            final_max_profit = x, max_profit
+        if i == min_profit_month:
+            final_min_profit = x, min_profit
+
+
+
+    # Final Output
+
+    print('Financial Analysis')
+    print('------------------------------------------')
+    print('Total Months: ' + str(total_months))
+    print('Total: $' + str(total_profitlosses))
+    print('Average Change: $' + str(average_change))
+    print('Greatest Increase in Profits: ' + str(final_max_profit))
+    print('Greates Decrease in Profits: ' + str(final_min_profit))
+
+    output_path = os.path.join('..', 'analysis', 'PyBank_Analysis.txt')
+    with open(output_path, "w") as text_file:
+        print('Financial Analysis', file=text_file,)
+        print('------------------------------------------', file=text_file,)
+        print('Total Months: ' + str(total_months), file=text_file,)
+        print('Total: $' + str(total_profitlosses), file=text_file,)
+        print('Average Change: $' + str(average_change), file=text_file,)
+        print('Greatest Increase in Profits: ' + str(final_max_profit), file=text_file,)
+        print('Greates Decrease in Profits: ' + str(final_min_profit), file=text_file,)
+
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,85 @@
-print('Hello from PyPoll')
+import os
+import csv
+
+# Path to collect data from the Resources folder
+election_csv = os.path.join('..', 'Resources', 'election_data.csv')
+
+unique_candidates = []
+all_cadidates = []
+
+li_votes = 0
+khan_votes = 0
+correy_votes = 0
+otooley_votes = 0
+total_votes = 0
+
+def unique_candidates(list1):
+    list_set = set(list1)
+    unique_list = (list(list_set))
+    for x in unique_list:
+        print(x)
+
+
+with open(election_csv, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+
+    # Loop through the data
+    for row in csvreader:
+        #print(row)
+        total_votes += 1
+
+        candidate = row[2]
+
+        all_cadidates.append(candidate)
+
+        if candidate == 'Li':
+            li_votes += 1
+        if candidate == 'Khan':
+            khan_votes += 1
+        if candidate == 'Correy':
+            correy_votes += 1
+        if candidate == 'O\'Tooley':
+            otooley_votes += 1
+
+    li_vote_percent = "{:.2%}".format(li_votes / total_votes)
+    khan_vote_percent = "{:.2%}".format(khan_votes / total_votes)
+    correy_vote_percent = "{:.2%}".format(correy_votes / total_votes)
+    otooley_vote_percent = "{:.2%}".format(otooley_votes / total_votes)
+    
+    print('Election Results')
+    print('---------------------------')
+    print('Total Votes: '+str(total_votes))
+    print('---------------------------')
+    print('Li: '+ str(li_vote_percent) + ' , ' + str(li_votes))
+    print('Khan: '+ str(khan_vote_percent) + ' , ' + str(khan_votes))
+    print('Correy: '+ str(correy_vote_percent) + ' , ' + str(correy_votes))
+    print('O\'Tooley: '+ str(otooley_vote_percent) + ' , ' + str(otooley_votes))
+
+    print('---------------------------')
+    print('Winner: Khan')
+
+
+output_path = os.path.join('..', 'analysis', 'PyPoll_Analysis.txt')
+with open(output_path, "w") as text_file:
+    print('Election Results', file=text_file)
+    print('---------------------------', file=text_file,)
+    print('Total Votes: '+str(total_votes), file=text_file,)
+    print('---------------------------', file=text_file,)
+    print('Li: '+ str(li_vote_percent) + ' , ' + str(li_votes), file=text_file,)
+    print('Khan: '+ str(khan_vote_percent) + ' , ' + str(khan_votes), file=text_file,)
+    print('Correy: '+ str(correy_vote_percent) + ' , ' + str(correy_votes), file=text_file,)
+    print('O\'Tooley: '+ str(otooley_vote_percent) + ' , ' + str(otooley_votes), file=text_file,)
+    print('---------------------------', file=text_file,)
+    print('Winner: Khan', file=text_file,)    
+   
+
+
+
+
+        
+        
